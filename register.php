4bf6d1f84b9e8b3df5250b92ba15dac35a4e188f
Maaz Ahmed <elfilosof@protonmail.com>
2019-09-27 22:29:58 +0530

    pulse

--- register.php
+++ register.php
@@ -8,7 +8,7 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="./css/style.css" <title>Account Registration hgahbs</title>
+  <link rel="stylesheet" href="./css/style.css" <title>Account Registration</title>
 </head>
 
 <body>
