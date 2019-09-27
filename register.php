a9be27f3a1cfaf9a07ab71397bafb2145cc623d5
Maaz Ahmed <elfilosof@protonmail.com>
2019-09-27 22:22:41 +0530

    asdas

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css"
 
-    <title>Account Registration</title>
+    <title>Account Registration hgahbs</title>
   </head>
   <body>
     <div class="wrapper">
