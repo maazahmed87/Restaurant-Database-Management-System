e2aac742086a03ade5e1028cdc589e2f98020c02
Maaz Ahmed <elfilosof@protonmail.com>
2019-11-01 21:00:04 +0530

    fixed issue with cart

--- menu.php
+++ menu.php
@@ -29,7 +29,25 @@ switch($_GET["action"]) {
 				$_SESSION["cart_item"] = $itemArray;
 			}
 		}
-	break;
+    break;
+    case "insert":
+		if(!empty($_SESSION["cart_item"])) {
+                foreach($_SESSION["cart_item"] as $k => $v){
+                    $quantity1=$_SESSION["cart_item"][$k]["quantity"];
+                    $code1=$_SESSION["cart_item"][$k]["code"];
+                    $email1=$_SESSION["email"];
+                    $total=$_SESSION["cart_item"][$k]["price"];
+                    $ord_date1=date("Y-m-d");
+                    $ord_time1=date('H:i');
+                    $db_handle->insq("insert into orders (`email`,`ord_date`,`code`,`quantity`,`ord_time`,`total`) values ('$email1','$ord_date1','$code1','$quantity1','$ord_time1','$total')");    
+                }
+                
+		unset($_SESSION["cart_item"]);
+                
+                
+                
+    }
+    break;
 	case "remove":
 		if(!empty($_SESSION["cart_item"])) {
 			foreach($_SESSION["cart_item"] as $k => $v) {
@@ -145,12 +163,12 @@ if(isset($_SESSION["cart_item"])){
     $total_quantity = 0;
     $total_price = 0;
 ?>
-            <table class="tbl-cart" cellpadding="10" cellspacing="1">
+            <table class="tbl-cart" cellpadding="10" cellspacing="2">
                 <tbody>
                     <tr>
                         <th style="text-align:left;" width="30%">Name</th>
                         <th style="text-align:right;" width="5%">Quantity</th>
-                        <th style="text-align:right;" width="10%">Unit Price</th>
+                        <th style="text-align:right;" width="20%">Unit Price</th>
                         <th style="text-align:right;" width="10%">Price</th>
                         <th style="text-align:center;" width="5%">Remove</th>
                     </tr>
@@ -166,9 +184,9 @@ if(isset($_SESSION["cart_item"])){
                         <td style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
                         <td style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
                         <td style="text-align:center;"><a
-                                href="menu.php?action=remove&code=<?php echo $item["code"]; ?>"
-                                class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
-                                
+                                href="menu.php?action=remove&code=<?php echo $item["code"]; ?>"><img
+                                    src="icon-delete.png" alt="Remove Item" /></a></td>
+
                     </tr>
                     <?php
 				$total_quantity += $item["quantity"];
@@ -182,7 +200,7 @@ if(isset($_SESSION["cart_item"])){
                         <td align="right" colspan="2">
                             <strong><?php echo "$ ".number_format($total_price, 2); ?></strong>
                         </td>
-                        <td></td>
+
                     </tr>
                 </tbody>
             </table>
@@ -193,6 +211,10 @@ if(isset($_SESSION["cart_item"])){
             <?php 
 }
 ?>
+
+
+            <a id="btnorder" href="menu.php?action=insert&code=<?php echo $item["code"]; ?>">Place order</a>
+            
         </div>
     </div>
 </BODY>
